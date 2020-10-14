4878218207f5cae34f72e864bbc6b3011b1ff473
yassin zaghdoudi <yassinzaghdoudi@gmail.com>
2020-10-14 07:09:43 +0200

    ShowNestedJsonList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  IsSelected: boolean;
+  Api = 'assets/API/Data.json';
+  data$: Observable<any[]>;
   title = 'SubMenu';
+  constructor(private http: HttpClient) {
+    this.data$ = this.getJSON();
+  }
+  public getJSON(): Observable<any> {
+    return this.http.get(this.Api);
+  }
+
 }
